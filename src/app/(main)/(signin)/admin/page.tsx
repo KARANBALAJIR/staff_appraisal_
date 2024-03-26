bc8fbcaeaa8793e67335cc3e025aa7f73e9c0429
KARANBALAJIR <karanbalaji.r2021cseb@sece.ac.in>
2024-03-26 10:53:34 +0530

    admin dash

--- src/app/(main)/(signin)/admin/page.tsx
+++ src/app/(main)/(signin)/admin/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>admin</div>
+    <>
+      <div className='space-y-4'>
+        <div className='max-w-screen-xl w-full h-4/5 bg-[#f3f3] p-60  rounded-md shadow-md px-8  '>
+        </div>
+      </div>
+    </>
   )
 }
 
