44df47cf33a44501f307170c50b62d058f8d2bac
muruga21 <murugaperumal21062004@gmail.com>
2024-04-01 10:57:41 +0530

    checking admin dash

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ import '@/components/Styles/LeftNav.css'
 
 const LeftNavBar = () => {
   return (
-    <div className='flex flex-col max-w-[30rem] items-center p-4 shadow-md bg-[#f3f4f5] rounded-md m-1'>
+    <div className='flex flex-col w-[18rem] h-[100vh] items-center p-4 shadow-md bg-[#f3f4f5] rounded-md m-1'>
         <div className='flex gap-4 items-center w-full border-solid border-b-2 border-[#c0c0c0] pb-2'>
           <Image src={profileLogo} alt='profile logo' className='h-10 w-auto'/>
           <div>
