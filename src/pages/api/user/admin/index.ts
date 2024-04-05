93209abba15eb7bee778c051e56769df363e84d4
muruga21 <murugaperumal21062004@gmail.com>
2024-04-05 12:35:49 +0530

    fixed be admin list user

--- src/pages/api/user/admin/index.ts
+++ src/pages/api/user/admin/index.ts
@@ -15,10 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         if(req.method === 'GET'){
             try{
-                const users = await prisma.user.findMany({
-                    take:10,
-                    skip:10*0,
-                });
+                const users = await prisma.user.findMany();
                 return res.status(200).json(users)
             }
             catch(err : any){
