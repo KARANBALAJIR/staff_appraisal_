04a79f9e6dcc95933f6e3d3c99894e4bc72c0503
Cibiyanna <pkcibiyanna@gmail.com>
2024-04-09 11:25:02 +0530

    toggle three dot still bug for overflow is not resolved

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef, useState } from "react";
 import { getCookie } from "@/services/cookie.service";
 import axios from "axios";
-
+import '@/styles/global.css'
 const Table = () =>{
     const actionButton = useRef<HTMLButtonElement>(null);
     const toggleActionButton = useRef<HTMLDivElement>(null);
@@ -10,12 +10,13 @@ const Table = () =>{
     const toggleFilterButton = useRef<HTMLDivElement>(null);
     const [users,setUsers] = useState([]);
     const [filteredUsers,setFilteredUsers] = useState([]);
+    // const threeDotButton = useRef<HTMLButtonElement>(null);
+    // const toggleThreeDotButton = useRef<HTMLDivElement>(null);
+    const [threeDotButton,setThreeDotButton] = useState(-1);
 
     useEffect(()=>{
         fetchUsers()
     },[])
-
-    
 
     useEffect(()=>{
         const handleButtonClick = () => {
@@ -47,6 +48,11 @@ const Table = () =>{
             (filterButton.current) ? filterButton.current.removeEventListener('click', handleButtonClick) : "";
         };
     }, [filterButton])
+
+
+    useEffect(()=>{
+
+    },[threeDotButton])
 
     const fetchUsers = async () => {
         try {
@@ -146,7 +152,7 @@ const Table = () =>{
                                 </div>
                             </div>
                         </div>
-                        <div className="overflow-x-auto">
+                        <div className="overflow-auto no-scrollbar">
                             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
@@ -162,7 +168,7 @@ const Table = () =>{
                                         </th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody className="">
                                         {filteredUsers.map((user: any, index) => {
                                             return (
                                                 <>
@@ -174,19 +180,18 @@ const Table = () =>{
                                                         <td className="px-4 py-3">{user.designation}</td>
                                                         <td className="px-4 py-3">{user.role}</td>
                                                         <td className="px-4 py-3">{user.status}</td>
-                                                        <td className="px-4 py-3 flex items-center justify-end">
-                                                            <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown" className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
+                                                        <td className="px-4 py-3 flex items-center justify-end relative">
+                                                            <button onClick={()=>{
+                                                                threeDotButton === index ? setThreeDotButton(-1) : setThreeDotButton(index)}
+                                                                }  id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown" className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
                                                                 <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                                     <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                                                 </svg>
                                                             </button>
-                                                            <div id="apple-imac-27-dropdown" className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                                                            <div id="apple-imac-27-dropdown" className={`${threeDotButton === index ? '' : 'hidden'} absolute right-[40px] top-[20px] z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600`}>
                                                                 <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="apple-imac-27-dropdown-button">
                                                                     <li>
-                                                                        <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Show</a>
-                                                                    </li>
-                                                                    <li>
-                                                                        <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                                                                        <button  className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</button>
                                                                     </li>
                                                                 </ul>
                                                                 <div className="py-1">
