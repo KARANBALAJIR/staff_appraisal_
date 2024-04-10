5b9f217dcb6d82912b0f4515da240e90dad2fbe5
Cibiyanna <pkcibiyanna@gmail.com>
2024-04-10 14:41:42 +0530

    improve table filter options

--- src/app/(main)/(signin)/user-management/SearchBar.tsx
+++ src/app/(main)/(signin)/user-management/SearchBar.tsx
@@ -9,13 +9,13 @@ const SearchBar = (props :SearchProps) =>{
     const {searchTerm, setSearchTerm} = props;
     return(
         <>
-            <div className='flex relative w-64 bg-white rounded-xl'>
+            <div className='flex relative w-64 bg-white rounded-md'>
                 <input
                     type="text"
                     placeholder="Search"
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
-                    className="rounded-md w-full px-[5px] py-[5px] outline-0  focus:shadow-sm focus:border-2 focus:border-blue-500"
+                    className="border-gray-200 rounded-md w-full px-[5px] py-[5px] outline-0  focus:shadow-sm border-2 focus:border-blue-500"
                 />
                 <button className="absolute right-[7px] top-[6px] flex items-center" onClick={()=>setSearchTerm('')}>
                     <span className="material-icons-sharp">
