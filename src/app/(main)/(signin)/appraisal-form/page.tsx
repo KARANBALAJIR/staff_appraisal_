2e8a4d99e21e128fb2d874fd3ed9b0673d778fa6
Cibiyanna <pkcibiyanna@gmail.com>
2024-04-23 10:24:57 +0530

    remove top navbar so full screen view of form

--- src/app/(main)/(signin)/appraisal-form/page.tsx
+++ src/app/(main)/(signin)/appraisal-form/page.tsx
@@ -1,19 +1,21 @@
 "use client"
 import Associate_form from "@/components/associate/Associate_form"
+import TabN14 from "@/components/associate/TabN1"
+
 
 
 export default function Appraisal_Page() {
     return (
-        <div className="flex flex-col justify-between h-full p-[10px]">
+        <div className="flex flex-col justify-between p-[10px]">
             <div className="">
                 <div className="p-[10px] text-center border-b-1 border-b-gray-400 text-xl font-semibold">Assistant Professor Form</div>
-                <div className="p-[10px]">
-                    <text>Helloworld</text>
+                <div className="p-[10px] h-[calc(100vh-140px)] overflow-y-auto">
+                    <text><TabN14/></text>
                 </div>
             </div>
            
             <div className="border-t-1 border-t-gray-400">
-                <div className="flex justify-between items-center p-[10px] ">
+                <div className="flex justify-between items-center p-[10px]">
                     <button className="rounded-xl border-2 border-blue-400  px-[15px] py-[10px]">back</button>
                     <button className="bg-blue-400 text-white px-[15px] py-[10px] rounded-xl">next</button>
                 </div>
--- src/app/(main)/(signin)/layout.tsx
+++ src/app/(main)/(signin)/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState  } from "react";
+import React, { useEffect, useState } from "react";
 import { getCookie } from "@/services/cookie.service";
 import axios from "axios";
 import { updateUserDetails } from "@/redux/userSlice";
@@ -10,32 +10,32 @@ import '@/components/Styles/index.css'
 import { UserType } from "@prisma/client";
 import { usePathname } from "next/navigation";
 
-export default function SignedInLayout({children}: Readonly<{children:React.ReactNode}>){
+export default function SignedInLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     const [sideBarOpen, setSideBarOpen] = useState(true);
     const [arrow_ios, setArrow_ios] = useState('arrow_forward_ios')
     const [role, setRole] = useState<UserType>(UserType.ANONYMOUS);
-    const [isLoading,setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const router = useRouter();
     const pathname = usePathname();
     const [defaultIndex, setDefaultIndex] = useState(0);
 
     const allRoles: Record<UserType, string[]> = {
-        'ANONYMOUS':['anonymous'],
+        'ANONYMOUS': ['anonymous'],
         'STAFF': ['appraisal-form'],
-        'HOD': ['appraisal-form','approval-form'],
+        'HOD': ['appraisal-form', 'approval-form'],
         'MASTER': ['approval-form'],
         'ADMIN': ['user-management', 'approval-form'],
 
     }
-    
+
     useEffect(() => {
         checkRole();
     }, [])
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         const userAccess = allRoles[role];
         router.push(`${userAccess[0]}`)
-    },[role])
+    }, [role])
 
     const checkRole = async () => {
         try {
@@ -56,97 +56,97 @@ export default function SignedInLayout({children}: Readonly<{children:React.Reac
             console.log(err.message);
         }
     }
-    
-    return(
+
+    return (
         <>
-                <div className="flex flex-row bg-white" >
-                    <div className='h-screen'>
-                        <div className={`${sideBarOpen === true ? ' w-[12.5rem]' : 'w-[5.5rem]'}   transition-all duration-200 ease-in relative `}>
-                            <div className='h-[60px] flex  items-center'>
-                                <div className="flex pl-[10px] items-center w-full h-[50px]">
-                                    <button
+            <div className="flex flex-row bg-white" >
+                <div className='h-screen'>
+                    <div className={`${sideBarOpen === true ? ' w-[12.5rem]' : 'w-[5.5rem]'}   transition-all duration-200 ease-in relative `}>
+                        <div className='h-[60px] flex  items-center'>
+                            <div className="flex pl-[10px] items-center w-full h-[50px]">
+                                <button
                                     className='flex items-center w-[50px] h-[50px] justify-center rounded-full hover:bg-gray-200 duration-200 ease-in'
-                                        onClick={() =>
-                                            setSideBarOpen(!sideBarOpen)
-                                        }><span className="material-icons-sharp rounded-full ">
-                                            menu
-                                        </span>
-                                    </button>
-                                </div>
-                                
+                                    onClick={() =>
+                                        setSideBarOpen(!sideBarOpen)
+                                    }><span className="material-icons-sharp rounded-full ">
+                                        menu
+                                    </span>
+                                </button>
                             </div>
-                            <div className='flex mt-[20px]'>
-                                <div className={`${sideBarOpen === true ? 'w-[180px]' : 'w-[65px]'} flex flex-col gap-y-4 items-center duration-200 ease-in`}>
-                                    {
-                                        allRoles[role].map((item,index)=>{
-                                            return(
-                                                <>
-                                                    {  (item === 'user-management' ) ? 
-                                                            <Link key={index} href='/user-management' onClick={() => { }} className={`px-[16px] py-[8px] ${pathname === '/user-management' ? 'bg-blue-400 text-white' : 'bg-gray-100 text-gray-700'} rounded-tr-xl rounded-br-xl w-full flex flex-row gap-4 items-center duration-200 ease-in`}>
-                                                                <span className={`${sideBarOpen === false ? '' : ''} material-icons-sharp `}>manage_accounts</span>
-                                                                <text className={` text-lg font-normal duration-200 ease-in ${sideBarOpen === true ? ' opacity-100' : ' opacity-0'}`}>manage</text>
-                                                            </Link> 
-                                                        :  <></> 
-                                                    }
 
-                                                    {  (item === 'approval-form') ?
-                                                        <Link key={index} href='/approval-form' onClick={() => { }} className={` px-[16px] py-[8px]  ${pathname === '/approval-form' ? 'bg-blue-400 text-white' : 'bg-gray-100 text-gray-700'}  rounded-tr-xl rounded-br-xl w-full  flex flex-row gap-4 items-center duration-200 ease-in`}>
-                                                                    <span className={`${sideBarOpen === false ? '' : ''} material-icons-sharp `}>approval</span>
-                                                                    <text className={`  text-lg font-normal duration-200 ease-in ${sideBarOpen === true ? ' opacity-100' : ' opacity-0'}`}>Approval</text>
-                                                            </Link> 
-                                                        : <></>
-                                                    }    
+                        </div>
+                        <div className='flex mt-[20px]'>
+                            <div className={`${sideBarOpen === true ? 'w-[180px]' : 'w-[65px]'} flex flex-col gap-y-4 items-center duration-200 ease-in`}>
+                                {
+                                    allRoles[role].map((item, index) => {
+                                        return (
+                                            <>
+                                                {(item === 'user-management') ?
+                                                    <Link key={index} href='/user-management' onClick={() => { }} className={`px-[16px] py-[8px] ${pathname === '/user-management' ? 'bg-blue-400 text-white' : 'bg-gray-100 text-gray-700'} rounded-tr-xl rounded-br-xl w-full flex flex-row gap-4 items-center duration-200 ease-in`}>
+                                                        <span className={`${sideBarOpen === false ? '' : ''} material-icons-sharp `}>manage_accounts</span>
+                                                        <text className={` text-lg font-normal duration-200 ease-in ${sideBarOpen === true ? ' opacity-100' : ' opacity-0'}`}>manage</text>
+                                                    </Link>
+                                                    : <></>
+                                                }
 
-                                                    {(item === 'appraisal-form') ?
-                                                        <Link key={index} href="/appraisal-form" className={`px-[16px] py-[8px] 3 rounded-tr-xl rounded-br-xl  ${pathname === '/appraisal-form' ? 'bg-blue-400 text-white' : 'bg-gray-100 text-gray-700'}   w-full flex flex-row gap-4 items-center duration-200 ease-in text-black`}>
-                                                            <span className="material-icons-sharp">insert_drive_file</span>
-                                                            <text className={` text-lg font-normal duration-200 ease-in ${sideBarOpen === true ? ' opacity-100 ' : ' opacity-0 '}`}>Form</text>
-                                                        </Link> : <></>
-                                                    }    
-                                                    {(item === 'anonymous') ?
-                                                        <Link key={index} href="/anonymous" className={`px-[16px] py-[8px] 3 rounded-tr-xl rounded-br-xl  ${pathname === '/anonymous' ? 'bg-blue-400 text-white' : 'bg-gray-100 text-gray-700'}   w-full flex flex-row gap-4 items-center duration-200 ease-in text-black`}>
-                                                            <span className="material-icons-sharp">no_accounts</span>
-                                                            <text className={` text-lg font-normal duration-200 ease-in ${sideBarOpen === true ? ' opacity-100 ' : ' opacity-0 '}`}>Anonymous</text>
-                                                        </Link> : <></>
-                                                    }    
-                                                </>
-                                            )
-                                        })
-                                        }
+                                                {(item === 'approval-form') ?
+                                                    <Link key={index} href='/approval-form' onClick={() => { }} className={` px-[16px] py-[8px]  ${pathname === '/approval-form' ? 'bg-blue-400 text-white' : 'bg-gray-100 text-gray-700'}  rounded-tr-xl rounded-br-xl w-full  flex flex-row gap-4 items-center duration-200 ease-in`}>
+                                                        <span className={`${sideBarOpen === false ? '' : ''} material-icons-sharp `}>approval</span>
+                                                        <text className={`  text-lg font-normal duration-200 ease-in ${sideBarOpen === true ? ' opacity-100' : ' opacity-0'}`}>Approval</text>
+                                                    </Link>
+                                                    : <></>
+                                                }
 
-                                </div>
+                                                {(item === 'appraisal-form') ?
+                                                    <Link key={index} href="/appraisal-form" className={`px-[16px] py-[8px] 3 rounded-tr-xl rounded-br-xl  ${pathname === '/appraisal-form' ? 'bg-blue-400 text-white' : 'bg-gray-100 text-gray-700'}   w-full flex flex-row gap-4 items-center duration-200 ease-in text-black`}>
+                                                        <span className="material-icons-sharp">insert_drive_file</span>
+                                                        <text className={` text-lg font-normal duration-200 ease-in ${sideBarOpen === true ? ' opacity-100 ' : ' opacity-0 '}`}>Form</text>
+                                                    </Link> : <></>
+                                                }
+                                                {(item === 'anonymous') ?
+                                                    <Link key={index} href="/anonymous" className={`px-[16px] py-[8px] 3 rounded-tr-xl rounded-br-xl  ${pathname === '/anonymous' ? 'bg-blue-400 text-white' : 'bg-gray-100 text-gray-700'}   w-full flex flex-row gap-4 items-center duration-200 ease-in text-black`}>
+                                                        <span className="material-icons-sharp">no_accounts</span>
+                                                        <text className={` text-lg font-normal duration-200 ease-in ${sideBarOpen === true ? ' opacity-100 ' : ' opacity-0 '}`}>Anonymous</text>
+                                                    </Link> : <></>
+                                                }
+                                            </>
+                                        )
+                                    })
+                                }
 
                             </div>
+
                         </div>
                     </div>
-                    <div className="h-screen flex-1">
-                    <div className="h-[60px]">
-                            <div className="flex h-full items-center justify-between z-10">
-                                <div className="ml-[20px]">
-                                    <h1 className="text-xl font-semibold ">STAFF APPRAISAL</h1>
-                                </div>
-                                <div className='flex gap-[20px] items-center p-[16px] h-full relative'>
-                                    <button>
-                                        <span className="material-icons-sharp text-black">notifications</span>
-                                    </button>
-                                    {/* <button className='' onClick={() => setUserIconClick(!usericonClick)}>
+                </div>
+                <div className="h-screen flex-1">
+                    {/* <div className="h-[60px]">
+                        <div className="flex h-full items-center justify-between z-10">
+                            <div className="ml-[20px]">
+                                <h1 className="text-xl font-semibold ">STAFF APPRAISAL</h1>
+                            </div>
+                            <div className='flex gap-[20px] items-center p-[16px] h-full relative'>
+                                <button>
+                                    <span className="material-icons-sharp text-black">notifications</span>
+                                </button>
+                                <button className='' onClick={() => setUserIconClick(!usericonClick)}>
                                         <span className="material-icons-sharp text-black">account_circle</span>
                                     </button>
 
                                     <div className={`${usericonClick === false ? 'opacity-0  -right-[400px]' : 'opacity-100   right-[15px]'} overflow-hidden top-[90px] shadow-md absolute  bg-black z-20 rounded-2xl duration-[0.3s] ease-in`}>
                                         <UserProfilePopUp />
-                                    </div> */}
-                                </div>
+                                    </div>
                             </div>
                         </div>
-                    <div className='rounded-tl-3xl h-[calc(100vh-60px)] overflow-hidden p-[15px] pb-0 shadow-inner home no-scrollbar duration-200 ease-in bg-gray-100 scroll-smooth z-10'>
+                    </div> */}
+                    <div className=' h-[calc(100vh)] overflow-hidden p-[15px] py-0 shadow-inner home no-scrollbar duration-200 ease-in bg-gray-100 scroll-smooth z-10'>
                         <div className="bg-white rounded-xl h-full overflow-y-auto no-scrollbar">
-                          {children}
+                            {children}
                         </div>
                     </div>
-                      
-                    </div>
+
                 </div>
+            </div>
         </>
     )
 }
