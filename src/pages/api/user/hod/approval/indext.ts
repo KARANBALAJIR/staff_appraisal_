f313687bd52d2b13668d1d6fdc5eac14a0567e2c
Cibiyanna <pkcibiyanna@gmail.com>
2024-06-01 10:32:29 +0530

    working on approval of hod

---
+++ src/pages/api/user/hod/approval/indext.ts
@@ -0,0 +1,47 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { checkAuthentication } from "../..";
+import { PrismaClient, FormStatus } from '@prisma/client';
+const { ObjectId } = require('mongoose');
+const prisma = new PrismaClient();
+
+import assoForm from '@/lib/data/associateStructure.json'
+import assiForm from '@/lib/data/Assistant_form_data.json'
+import professorForm from '@/lib/data/Professor_form_data.json'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    await checkAuthentication(req, res, async function () {
+
+        if(req.method === 'GET'){
+            try{
+                // const hod_request
+            }catch(err : any){
+
+            }
+        }
+
+        if (req.method === 'POST') {
+            try {
+                const { form_id } = req.query;
+                const { approval_status } = req.body;
+
+                const newFormData = await prisma.userCreationForm.update({
+                    where: {
+                        id: form_id as string,
+                    },
+                    data: {
+                        hod_status:approval_status,
+                    }
+                });
+                return res.status(200).json({ status: "success", message: "Successfully created form", data: newForm })
+
+            }
+            catch (error: any) {
+                return res.status(500).json({ status: "error", message: error.message })
+            }
+        }
+
+    })
+
+
+}   
